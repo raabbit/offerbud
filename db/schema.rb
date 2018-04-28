70572761c21faa1c98f586a761446fbe3277735f
raabbit <r_abbit@wp.pl>
2018-04-28 18:38:44 +0000

    scaffold ofert, zmiana paska nawigacyjnego

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180426201352) do
+ActiveRecord::Schema.define(version: 20180428161019) do
+
+  create_table "offers", force: :cascade do |t|
+    t.string   "description"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
